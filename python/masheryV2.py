4489ee938d16beb82cf0444a522d98e378aad590
jeremy <jpollock@mashery.com>
2015-03-02 12:50:11 -0800

    adding v2 and v3 connectors

---
+++ python/masheryV2.py
@@ -0,0 +1,22 @@
+import requests, json, hashlib, time
+
+apiHost = 'http://api.mashery.com'
+
+def get(siteId, apikey, secret, resource, params):
+    resourceEndpoint = '/v2/rest'
+    url = apiHost + resourceEndpoint + '/' + siteId + resource + '?apikey=' + apikey + '&sig=' + hash(apikey, secret) + params
+    response = requests.get(url)
+    return response.json()
+
+def post(siteId, apikey, secret, payload):
+    resourceEndpoint = '/v2/json-rpc'
+    headers = {"Content-type": "application/json"}
+    url = apiHost + resourceEndpoint + '/' + siteId + '?apikey=' + apikey + '&sig=' + hash(apikey, secret)
+    response = requests.post(url, headers=headers, data=payload)
+    return response.json()
+
+def hash(apikey, secret):
+    authHash = hashlib.md5();
+    temp = str.encode(apikey + secret + repr(int(time.time())))
+    authHash.update(temp)
+    return authHash.hexdigest()
