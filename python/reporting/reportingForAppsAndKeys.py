c648d3b80c2d940518b239ebfe20ca570c3fbc8a
jeremy <jpollock@mashery.com>
2015-03-04 06:50:20 -0800

    update to reportingForAppsAndKeys: change to the input arguments

--- python/reporting/reportingForAppsAndKeys.py
+++ python/reporting/reportingForAppsAndKeys.py
@@ -1,5 +1,5 @@
-import sys, datetime, urllib
-sys.path.append( '../../masheryapi/python' )
+import sys, datetime, urllib, argparse
+sys.path.append( '../lib/' )
 import masheryV2
 
 def dayGap(startDate, endDate):
@@ -27,7 +27,7 @@ def daysToReportOn(startDate, endDate):
     sevenDayGap = sevenDays(sevenDayGap[1], endDate)
     daysToReportOn.append(sevenDayGap)
   
-  if (dayGap(sevenDayGap[1], endDate) < 7): 
+  if (dayGap(sevenDayGap[1], endDate) < 7 and sevenDayGap[1] != endDate): 
     daysToReportOn.append([sevenDayGap[1], endDate])
 
   return daysToReportOn
@@ -39,28 +39,40 @@ def apiName(apis, apiId):
 
 def main(argv):
   
-  if (len(sys.argv) < 7 or len(sys.argv) > 8):
-    print 'Usage: python reportingForAppsAndKeys.py <Mashery V2 API Key> <Mashery V2 API Secret> <Mashery Area/Site ID> <Start Date> <End Date> <Output Filename> <List of APIs (optional)>'
+  parser = argparse.ArgumentParser()
+  parser.add_argument("apikey", type=str, help="Mashery V2 API Key")
+  parser.add_argument("secret", type=str, help="Mashery V2 API Secret")
+  parser.add_argument("siteId", type=str, help="Mashery Area/Site ID")
+  parser.add_argument("startDate", type=str, help="Start Date")
+  parser.add_argument("endDate", type=str, help="End Date")
+  parser.add_argument("outputFile", type=str, help="Output Filename")
+  parser.add_argument('--apis',  nargs='+', help='List of APIs by name, space separated')
+  parser.add_argument('--fields',  nargs='+', help='List of key/app fields to retrieve, space separated')
+  args = parser.parse_args()
+  
+  apikey = args.apikey
+  secret = args.secret
+  siteId = args.siteId
+  startDate = args.startDate
+  endDate = args.endDate
+  outputFile = args.outputFile
+
+  if dayGap(startDate, endDate) < 1:
+    print 'ERROR: endDate must be at least 1 day past startDate'
     return
 
-  apikey = sys.argv[1] 
-  secret = sys.argv[2]
-  siteId = sys.argv[3]
-  startDate = sys.argv[4]
-  endDate = sys.argv[5]
-  outputFile = sys.argv[6]
+  apis = args.apis
 
-  apis = []
+  if args.apis == None:
+    apis = []
 
-  if len(sys.argv) == 8:
-    apis = sys.argv[7]
-
+  # get list of APIs
   all_apis = masheryV2.post(siteId, apikey, secret, '{"method":"object.query","id":1,"params":["select * from services ITEMS 1000"]}')
 
   results = []
 
   dates = daysToReportOn(startDate, endDate)
-
+  print dates
   for api in all_apis['result']['items']:
     try:
         if (len(apis) > 0):
@@ -68,23 +80,36 @@ def main(argv):
 
         print 'Processing...' + api['name']
         for date in dates:
-          urlParams = '&start_date=' + urllib.quote_plus(date[0]) + '&end_date=' + urllib.quote_plus(date[1]) + '&format=json&limit=1000'          
+          urlParams = '&start_date=' + urllib.quote_plus(date[0]) + '&end_date=' + urllib.quote_plus(date[1]) + '&format=json&limit=1000'
           results.extend(masheryV2.get(siteId, apikey, secret, '/reports/calls/developer_activity/service/' + api['service_key'], urlParams))
 
     except ValueError:
         pass
 
   f = open(outputFile,'w')
-  f.write('Application,API ID,API Name,Key,Start Date,End Date,Successful,Blocked,Other,Total\n')
+  customFields = 'application.name'
+  if (args.fields != None):
+    customFields = ','.join(args.fields)
+
+  headers = customFields + ',service.name,service.id,key.apikey,startDate,endDate,successful,blocked,other,total\n'
+
+  f.write(headers)
   for result in results:
     try:
-
-        key = masheryV2.post(siteId, apikey, secret, '{"method":"object.query","id":1,"params":["select *, application from keys where apikey = \'' + result['serviceDevKey']  + '\'"]}')
+        key = masheryV2.post(siteId, apikey, secret, '{"method":"object.query","id":1,"params":["select ' + customFields + ' from keys where apikey = \'' + result['serviceDevKey']  + '\'"]}')
 
         application_name = '<UNKNOWN>'
-        if (key['result']['total_items'] > 0) :
-            application_name = key['result']['items'][0]['application']['name']
-        f.write('"' + application_name + '",' + result['serviceKey'] + ',"' + apiName(all_apis, result['serviceKey']) + '",'+ result['serviceDevKey']  + ',' + result['startDate']  + ',' + result['endDate']  + ','+ str(result['callStatusSuccessful'])  + ',' + str(result['callStatusBlocked']) + ',' + str(result['callStatusOther']) + ',' + str(result['callStatusSuccessful'] +  result['callStatusBlocked'] + result['callStatusOther']) + '\n')
+        if (key['result']['total_items'] > 0) :            
+            if (args.fields != None):
+              customFieldValues = ''
+              for field in args.fields:
+                splitFields = field.split('.')
+                if len(splitFields) == 2:
+                  customFieldValues = customFieldValues + '"' + key['result']['items'][0][splitFields[0]][splitFields[1]] + '",'
+                else:
+                  customFieldValues = customFieldValues + '"' + key['result']['items'][0][splitFields[0]] + '",'
+
+        f.write(customFieldValues + result['serviceKey'] + ',"' + apiName(all_apis, result['serviceKey']) + '",'+ result['serviceDevKey']  + ',' + result['startDate']  + ',' + result['endDate']  + ','+ str(result['callStatusSuccessful'])  + ',' + str(result['callStatusBlocked']) + ',' + str(result['callStatusOther']) + ',' + str(result['callStatusSuccessful'] +  result['callStatusBlocked'] + result['callStatusOther']) + '\n')
     except TypeError:
         pass
 
