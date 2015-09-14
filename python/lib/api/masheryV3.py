2642878dd19a7c91acf0f69dbee7cb20a214b4a1
jeremy <jpollock@mashery.com>
2015-09-14 11:57:37 -0700

    Adding update to v3 lib for support of PUT

--- python/lib/api/masheryV3.py
+++ python/lib/api/masheryV3.py
@@ -1,32 +1,35 @@
 import requests, json
 from requests.auth import HTTPBasicAuth
 
-apiHost = 'https://api.mashery.com'
-tokenEndpoint = '/v3/token'
-resourceEndpoint = '/v3/rest'
+class MasheryV3:
 
-def authenticate(apikey, secret, username, password, areaUuid):
-    payload = {'grant_type': 'password', 'username' : username, 'password'  : password, 'scope' : areaUuid} 
-    response = requests.post(apiHost + tokenEndpoint, auth=HTTPBasicAuth(apikey, secret), data=payload)
-    return response.json()['access_token']
+    def __init__(self):
+        self.protocol = 'https'
+        self.api_host = 'api.mashery.com'
+        self.token_endpoint = '/v3/token'
+        self.resource_endpoint = '/v3/rest'
 
+    def authenticate(self, apikey, secret, username, password, areaUuid):
+        payload = {'grant_type': 'password', 'username' : username, 'password'  : password, 'scope' : areaUuid} 
+        response = requests.post(self.protocol + '://' + self.api_host + self.token_endpoint, auth=HTTPBasicAuth(apikey, secret), data=payload)
+        return response.json()['access_token']
 
-def get(token, resource, params):
-    headers = {"Content-type": "application/json", "Authorization": 'Bearer ' + token}
-    response = requests.get(apiHost + resourceEndpoint + resource + '?' + params, headers=headers)
-    return response.json()
+    def get(self, token, resource, params):
+        headers = {"Content-type": "application/json", "Authorization": 'Bearer ' + token}
+        response = requests.get(self.protocol + '://' + self.api_host + self.resource_endpoint + resource + '?' + params, headers=headers)
+        return response.json()
 
-def put(token, resource, payload):
-    headers = {"Content-type": "application/json", "Authorization": 'Bearer ' + token}
-    response = requests.post(apiHost + resourceEndpoint + resource , headers=headers, data=json.dumps(payload))
-    return response.json()
+    def put(self, token, resource, params, payload):
+        headers = {"Content-type": "application/json", "Authorization": 'Bearer ' + token}
+        response = requests.put(self.protocol + '://' + self.api_host + self.resource_endpoint + resource + '?' + params, headers=headers, data=json.dumps(payload))
+        return response.json()
 
-def post(token, resource, payload):
-    headers = {"Content-type": "application/json", "Authorization": 'Bearer ' + token}
-    response = requests.post(apiHost + resourceEndpoint + resource , headers=headers, data=json.dumps(payload))
-    return response.json()
+    def post(self, token, resource, payload):
+        headers = {"Content-type": "application/json", "Authorization": 'Bearer ' + token}
+        response = requests.post(self.protocol + '://' + self.api_host + self.resource_endpoint + resource , headers=headers, data=json.dumps(payload))
+        return response.json()
 
-def delete(token, resource):
-    headers = {"Content-type": "application/json", "Authorization": 'Bearer ' + token}
-    response = requests.delete(apiHost + resourceEndpoint + resource , headers=headers)
-    return response.status_code
+    def delete(self, token, resource):
+        headers = {"Content-type": "application/json", "Authorization": 'Bearer ' + token}
+        response = requests.delete(self.protocol + '://' + self.api_host + self.resource_endpoint + resource , headers=headers)
+        return response.status_code
